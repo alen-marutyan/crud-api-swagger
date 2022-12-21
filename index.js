0cd5e3bf7fd0737fc08e4d73e92fb04de60f16ca
alen-marutyan <alen.marutyan@inbox.ru>
2022-12-21 23:41:03 +0400

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -8,6 +9,10 @@ const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 
+const corsOption = {
+    origin: ["https://api-docs-swagger1.onrender.com", "https://render.com" ],
+};
+app.use(cors(corsOption));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
